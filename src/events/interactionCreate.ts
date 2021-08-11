09c7d60b246ae3778f5733a540ff588ea2062f24
Avinash <avinashdhillor24@gmail.com>
2021-08-11 08:49:11 +0530

    test

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "interactionCreate",
+    async execute(interaction) {
+        // console.log(`Ready! ${client.user.username}`);
+
+        console.log(interaction);
+    },
+};
